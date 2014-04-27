eaf7c8d07863ee8ec8b4ba5ec9ad4f2d66b38bd9
MitsuoMasuda <masuda.mitsuo@gmail.com>
2014-04-28 00:48:41 +0900

    2014/04/28
    Analizeクラスの追加

---
+++ IndexDB/Analyze.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexDB
+{
+	public class Analyze
+	{
+		private string _dicDir = "";	// 辞書DBのパス
+
+		public Analyze(string dicDir)
+		{
+			this._dicDir = dicDir;
+		}
+
+		public ArrayList Execute(string sentence)
+		{
+			if (this._dicDir == "")
+			{
+				return null;
+			}
+
+			NMeCab.MeCabParam param = new NMeCab.MeCabParam();
+			param.DicDir = this._dicDir;
+			param.LatticeLevel = NMeCab.MeCabLatticeLevel.One;
+
+			NMeCab.MeCabTagger mecabTagger = NMeCab.MeCabTagger.Create(param);
+			string message = mecabTagger.ParseNBest(2, sentence);
+
+			Dictionary<string, string> dictionary = new Dictionary<string, string>();
+			foreach (string m in message.Split('\n'))
+			{
+				try
+				{
+					// 名詞のみ対象
+					if (m.Split('\t')[1].IndexOf("名詞") == 0)
+					{
+						dictionary.Add(m.Split('\t')[0], "A");
+					}
+				}
+				catch
+				{
+				}
+			}
+
+			ArrayList wordList = new ArrayList();
+			foreach (string key in dictionary.Keys)
+			{
+				wordList.Add(key);
+			}
+
+			return wordList;
+		}
+
+	}
+}
